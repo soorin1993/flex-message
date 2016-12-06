b467a055665f5ea2b79f96cfe604d0b88e4d406c
Soo Park <soorin1993@gmail.com>
2016-12-05 20:33:34 -0700

    flex message done

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+var movementStrength = 25;
+var height = movementStrength / $(window).height();
+var width = movementStrength / $(window).width();
+$("#snowbg").mousemove(function(e){
+    var pageX = e.pageX - ($(window).width() / 2);
+    var pageY = e.pageY - ($(window).height() / 2);
+    var newvalueX = width * pageX * -1 - 25;
+    var newvalueY = height * pageY * -1 - 50;
+    console.log(newvalueY);
+    $('#snowbg').css("background-position", newvalueX+"px     "+newvalueY+"px");
+});
+});
